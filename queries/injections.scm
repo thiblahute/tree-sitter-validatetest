8db6b1e5826ebc6fbf44eb3b7579e9d9a065b979
Thibault Saunier <tsaunier@igalia.com>
2026-01-15 14:55:14 -0300

    queries: Add injection support for caps fields and GstCaps typed values

--- queries/injections.scm
+++ queries/injections.scm
@@ -20,3 +20,19 @@
  (#eq? @_field_name "expected-issues")
  (#set! injection.language "validatetest")
  (#set! injection.include-children))
+
+; Typed GstCaps values contain GstStructure/caps syntax
+((typed_value
+  type: (type_name) @_type
+  value: (value (string (string_inner) @injection.content)))
+ (#eq? @_type "GstCaps")
+ (#set! injection.language "validatetest")
+ (#set! injection.include-children))
+
+; Field named 'caps' with string value contains caps syntax
+((field
+  name: (field_name (identifier) @_field_name)
+  value: (field_value (value (string (string_inner) @injection.content))))
+ (#eq? @_field_name "caps")
+ (#set! injection.language "validatetest")
+ (#set! injection.include-children))
